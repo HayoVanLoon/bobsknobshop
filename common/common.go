e5d711fdd473598be6bae47c284bc5038a43c5f7
Hayo van Loon <hayovanloon@gmail.com>
2019-10-19 18:01:12 +0200

    add method

--- common/common.go
+++ common/common.go
@@ -1,14 +1,15 @@
 package common
 
-type ServiceImplementationDetails interface {
+type ServiceImplementation interface {
 	ParentName() string
 	ParentVersion() string
 	Name() string
 	Port() int
 	Active() bool
+	Service() string
 }
 
-type serviceImplementationDetails struct {
+type serviceImplementation struct {
 	parentName    string
 	parentVersion string
 	name          string
@@ -16,26 +17,30 @@ type serviceImplementationDetails struct {
 	active        bool
 }
 
-func (s serviceImplementationDetails) ParentName() string {
+func (s serviceImplementation) ParentName() string {
 	return s.parentName
 }
 
-func (s serviceImplementationDetails) ParentVersion() string {
+func (s serviceImplementation) ParentVersion() string {
 	return s.parentVersion
 }
 
-func (s serviceImplementationDetails) Name() string {
+func (s serviceImplementation) Name() string {
 	return s.name
 }
 
-func (s serviceImplementationDetails) Port() int {
+func (s serviceImplementation) Port() int {
 	return s.port
 }
 
-func (s serviceImplementationDetails) Active() bool {
+func (s serviceImplementation) Active() bool {
 	return s.active
 }
 
-func NewServiceImplementationDetails(parentName, parentVersion, name string, port int, active bool) ServiceImplementationDetails {
-	return serviceImplementationDetails{parentName, parentVersion, name, port, active}
+func (s serviceImplementation) Service() string {
+	return s.parentName + "-" + s.parentVersion + "-service-" + s.name
+}
+
+func NewServiceImplementationDetails(parentName, parentVersion, name string, port int, active bool) ServiceImplementation {
+	return serviceImplementation{parentName, parentVersion, name, port, active}
 }
