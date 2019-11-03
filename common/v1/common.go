118d4c0feac99fc593d5cddd76655a5305277cac
Hayo van Loon <hayovanloon@gmail.com>
2019-11-03 14:03:51 +0100

    remove stuff

--- common/v1/common.go
+++ common/v1/common.go
@@ -5,51 +5,6 @@ import (
 	"log"
 )
 
-type ServiceImplementation interface {
-	ParentName() string
-	ParentVersion() string
-	Name() string
-	Port() int
-	Active() bool
-	Service() string
-}
-
-type serviceImplementation struct {
-	parentName    string
-	parentVersion string
-	name          string
-	port          int
-	active        bool
-}
-
-func (s serviceImplementation) ParentName() string {
-	return s.parentName
-}
-
-func (s serviceImplementation) ParentVersion() string {
-	return s.parentVersion
-}
-
-func (s serviceImplementation) Name() string {
-	return s.name
-}
-
-func (s serviceImplementation) Port() int {
-	return s.port
-}
-
-func (s serviceImplementation) Active() bool {
-	return s.active
-}
-
-func (s serviceImplementation) Service() string {
-	return s.parentName + "-" + s.parentVersion + "-service-" + s.name
-}
-
-func NewServiceImplementation(parentName, parentVersion, name string, port int, active bool) ServiceImplementation {
-	return serviceImplementation{parentName, parentVersion, name, port, active}
-}
-
 // Provides a connection-closing function
 func CloseConnFn(conn *grpc.ClientConn) func() {
 	return func() {
--- common/v1/go.mod
+++ common/v1/go.mod
@@ -1,9 +1,9 @@
-module github.com/HayoVanLoon/bobsknobshop/common
+module github.com/HayoVanLoon/bobsknobshop/common/v1
 
 go 1.13
 
 require (
-	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271 // indirect
+	golang.org/x/net v0.0.0-20191101175033-0deb6923b6d9 // indirect
 	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20191028173616-919d9bdd9fe6 // indirect
